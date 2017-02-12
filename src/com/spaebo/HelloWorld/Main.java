ce42aecbf765b051101385826636a192e07cc4b8
Alex Sawtschuk <a.saw@gmx.net>
2017-02-12 18:30:38 +0100

    Readability hoffentlich improved

--- src/com/spaebo/HelloWorld/Main.java
+++ src/com/spaebo/HelloWorld/Main.java
@@ -15,7 +15,15 @@ public class Main{
      * A universal greeting sent by the greeters.
      * @param greeters The greeting entity(s)
      */
-	private static void hello(String[] greeters) {
-        Arrays.stream(greeters).forEach(x -> System.out.println(x));
+	private static void hello(String... greeters) {
+        Arrays.stream(greeters).forEach(greeter -> hello(greeter));
 	}
+    
+    /**
+     * A single greeting sent by a greeter.
+     * @param greeter The greeting entity
+     */ 
+    private static void hello(String greeter) {
+        System.out.println(greeter + " sagt hallo");
+    }
 }
