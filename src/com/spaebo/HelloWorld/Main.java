5c89415437bde1d161c892fab1ee226966d027c8
Yannick Kaupe <yannick.kaupe@gmail.com>
2017-02-12 16:55:51 +0100

    Philipp, da hat Heiko uns so eine Steilvorlage gegeben und du nutzt sie nicht komplett. Andererseits haettest du sie auch besser kommentieren koennen Heiko. Nicht jeder ist so advanced :D

--- src/com/spaebo/HelloWorld/Main.java
+++ src/com/spaebo/HelloWorld/Main.java
@@ -5,16 +5,16 @@ package com.spaebo.HelloWorld;
 public class Main{
 
     public static void main(String[] args){
-        System.out.println("Hello World");
-        yannickHallo();
-		hello("highc0");
-		hello("headegg");
+        hello("yanniqq","highc0","headegg");
+		
     }
     
-    private static void yannickHallo() {
-    	System.out.println("Yannick sagt auch allen 'Hallo!'");
-    }
+    
 
+    /**
+     * A universal greeting sent by the greeters.
+     * @param greeters The greeting entity(s)
+     */
 	private static void hello(String... greeters) {
 		for (String greeter : greeters){
 			System.out.println(greeter + ": Hello World");
