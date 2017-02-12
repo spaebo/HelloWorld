eaa6db76a6ed58d8f908e8e6ce7807336bcdc511
Yannick Kaupe <yannick.kaupe@gmail.com>
2017-02-12 16:25:52 +0100

    Reshaped project. Extended gitignore to ignore Eclipse specific files

---
+++ src/com/spaebo/HelloWorld/Main.java
@@ -0,0 +1,15 @@
+package com.spaebo.HelloWorld;
+
+
+
+public class Main{
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        yannickHallo();
+    }
+    
+    private static void yannickHallo() {
+    	System.out.println("Yannick sagt auch allen 'Hallo!'");
+    }
+}
