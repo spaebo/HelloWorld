522ed0625963187902461c542d861283fd2ba263
Heiko Nickerl <info@heiko-nickerl.com>
2017-02-12 16:38:21 +0100

    added greetings from highc0

--- src/com/spaebo/HelloWorld/Main.java
+++ src/com/spaebo/HelloWorld/Main.java
@@ -7,9 +7,16 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Hello World");
         yannickHallo();
+		hello("highc0");
     }
     
     private static void yannickHallo() {
     	System.out.println("Yannick sagt auch allen 'Hallo!'");
     }
+
+	private static void hello(String... greeters) {
+		for (String greeter : greeters){
+			System.out.println(greeter + ": Hello World");
+		}
+	}
 }
